8e14ddcb8f77da08e64c7a3f2cb0f22e2e1b5986
Rainmaker <106910249+rainmaker-2@users.noreply.github.com>
2026-07-31 14:46:16 +0700

    Create layout.js

---
+++ layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Robot Scraper SEO Game',
+  description: 'Memindai event game otomatis untuk SEO top up',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  )
+}
